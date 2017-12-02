525e026ce68a8464349836372f8deb2579095110
Jeff Moore <jeffmoore@Jeffs-MacBook-Pro.local>
2017-12-01 21:49:21 -0800

    Added comments to my code

--- pybank/main.py
+++ pybank/main.py
@@ -1,6 +1,8 @@
+#Completing the initial imports
 import os
 import csv
 
+#Creating all of my variables
 month_year = []
 revenue = []
 row_current = []
@@ -13,9 +15,11 @@ total_revenue = 0
 number_of_months = 0
 current_row = 0
 
+#Building links to files and their locations
 budget_data1 = os.path.join('../pythonstuff/budget_data_1.csv')
 budget_data2 = os.path.join('../pythonstuff/budget_data_2.csv')
 
+#Reading files
 with open(budget_data1, newline="") as csvfile1:
     csvreader = csv.reader(csvfile1, delimiter=",")
     next(csvreader, None)
@@ -34,12 +38,16 @@ with open(budget_data2, newline="") as csvfile2:
         month_year.append(row_current[0])
         revenue.append(row_current[1])
 
+#Calculating total revenue
 for dollar in revenue:
     total_revenue = total_revenue + int(dollar)
 
+#Calculating number of months and changing variable type
 number_of_months = len(month_year)
 total_revenue = int(total_revenue)
 
+#Looping to determine high and low monthly differentials
+#Also determining total monthly differential
 for rev in revenue:
     monthly_rev_diff = int(rev) - previous_rev
     if monthly_rev_diff > 0 and monthly_rev_diff > rev_high_diff:
@@ -55,14 +63,17 @@ for rev in revenue:
     previous_rev = int(rev)
     current_row = current_row + 1
     total_rev_diff = total_rev_diff + monthly_rev_diff
-    
+
+#Calculating average change per month over the entire dataset (from 2009 to 2016)    
 avg_month_change_rev = total_rev_diff / number_of_months
 
+#Formating the output
 total_revenue = "$ %8.2f"% total_revenue
 rev_high_diff = "$ %8.2f"% rev_high_diff
 rev_low_diff = "$ %8.2f"% rev_low_diff
 avg_month_change_rev = "$ %8.2f"% avg_month_change_rev
 
+#Printing the output to the screen
 print("Financial Analysis")
 print("-------------------------------------")
 print("Total Number of Months: " + str(number_of_months))
@@ -71,6 +82,7 @@ print("Average Revenue Change: " + avg_month_change_rev)
 print("Greatest Increase in Revenue: " + rev_high_month_year + " " +  rev_high_diff)
 print("Greatest Decrease in Revenue: " + rev_low_month_year + " " +  rev_low_diff)
 
+#Printing the output to the output folder
 output_path = os.path.join('../pythonstuff/output', 'budget_analysis.csv')
 
 with open(output_path, "w", newline="") as csvfile:
--- pypoll/main.py
+++ pypoll/main.py
@@ -1,9 +1,12 @@
+#Completing the initial imports
 import os
 import csv
 
+#Building links to files and their locations
 election_data1 = os.path.join('../pythonstuff/election_data_1.csv')
 election_data2 = os.path.join('../pythonstuff/election_data_2.csv')
 
+#Creating all of my variables
 voter_id = []
 county_name = []
 candidate_table = []
@@ -16,6 +19,7 @@ max_vote = 0
 total_number_votes = 0
 vote_count = 0
 
+#Reading files
 with open(election_data1, newline="") as csvfile1:
     csvreader = csv.reader(csvfile1, delimiter=",")
     next(csvreader, None)
@@ -36,13 +40,18 @@ with open(election_data2, newline="") as csvfile1:
         county_name.append(row_current[1])
         candidate_table.append(row_current[2])
 
+#Calculating the total number of votes
 total_number_votes = int(len(voter_id))
 
+#Building a list of unique candidates using the set function
 for name in candidate_table:
     set_candidates.add(name)
 
+#Converting my set back to a list because sets and loops don't mix
 list_candidates = list(set_candidates)
 
+#Looping thru the list of candidates to gather total votes per candidate info
+#Calculating percentage of total information
 for name in list_candidates:
     vote_count = candidate_table.count(name)
     if vote_count > max_vote:
@@ -55,6 +64,7 @@ for name in list_candidates:
     number_votes.append(vote_count)
     percent_votes.append(percent_of_vote)
 
+#Removing \n syntax from each list string...to make the output more pleasing to the eyeball
 list_candidates[0] = list_candidates[0].strip()
 list_candidates[1] = list_candidates[1].strip()
 list_candidates[2] = list_candidates[2].strip()
@@ -66,6 +76,7 @@ list_candidates[7] = list_candidates[7].strip()
 list_candidates[8] = list_candidates[8].strip()
 list_candidates[9] = list_candidates[9].strip()
 
+#Formatting the output for the screen
 print("Election Results")
 print("-------------------------------------")
 print("Total Votes: " + str(total_number_votes))
@@ -84,6 +95,7 @@ print("-------------------------------------")
 print("Winner: " + save_winner)
 print("-------------------------------------")
 
+#Formating the output for the output file
 output_path = os.path.join('../pythonstuff/output', 'election_analysis.csv')
 
 with open(output_path, "w", newline="") as csvfile:
